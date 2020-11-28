92ea3a282759dae43bb10241aec0b3658ea38dbb
gowthamsrikrish <docker@Docker.4nrm0lukjadudn0zu5f0psfjcg.bx.internal.cloudapp.net>
2020-11-28 16:01:54 +0000

    random quote bot is working

--- get-quote.py
+++ get-quote.py
@@ -1,11 +1,13 @@
-def primary():
-   print("Keep it logically awesome.")
+def main():
+    import random
+    print("Keep it logically awesome.")
 
-  #f = open("quotes.txt")
-  #quotes = f.readlines()
-  #f.close()
+    f = open("quotes.txt")
+    quotes = f.readlines()
+    f.close()
+    last = 13
+    rnd = random.randint(0, last)
+    print(quotes[rnd])
 
-  #print(quotes)
-
-if __name__== "__primary__":
-  primary()
+if __name__== "__main__":
+  main()
